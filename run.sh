9772000205aa5aca1437b8399fe4971022cf5101
nicephil <nicephil@gmail.com>
2020-03-05 22:32:24 +0800

    init

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -tid --name youtube-dl-web -p 5555:5555 -v /home/llwang/repos/dockers/youtube-dl-web/downloads:/youtube_dl oldiy/youtube-dl-web
+docker run -tid --restart=always --name youtube-dl-web -p 5555:5555 -v /home/llwang/repos/dockers/youtube-dl-web/downloads:/youtube_dl oldiy/youtube-dl-web
